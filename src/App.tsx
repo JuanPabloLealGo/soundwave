03d8508902b10d8508712fce8bf301f3683c6c7b
Juan Leal <jplealg@unal.edu.co>
2022-09-24 13:50:07 -0500

    feat: user authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,19 @@
 import React from 'react'
 import { useRoutes } from 'react-router-dom';
 import MainLayout from './components/layouts/MainLayout';
-
 import Home from './components/pages/Home'
 import NotFound from './components/pages/NotFount';
 import Playlist from './components/pages/Playlist';
 import Playlists from './components/pages/Playlists';
-
-/*
-const App = () => {
-  return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/playlists' element={<MainLayout />} >
-        <Route index element={<Playlists />} />
-        <Route path=':id' element={<Playlist />} />
-      </Route>
-      <Route path='*' element={<NotFound />} />
-    </Routes>
-  );
-}
-*/
+import useAuth from './hooks/useAuth';
 
 const App = () => {
+  const isAuthenticated = useAuth()
+
   const element = useRoutes([
     {
       path: '/',
-      element: <Home />
+      element: <Home isAuthenticated={isAuthenticated} />
     },
     {
       path: '/playlists',
@@ -44,5 +31,21 @@ const App = () => {
 
   return element
 }
+
+// OTHER WAY:
+/*
+const App = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/playlists' element={<MainLayout />} >
+        <Route index element={<Playlists />} />
+        <Route path=':id' element={<Playlist />} />
+      </Route>
+      <Route path='*' element={<NotFound />} />
+    </Routes>
+  );
+}
+*/
 
 export default App;
